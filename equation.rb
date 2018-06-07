aea988edf6295657ae973f6588793b52713b9bfe
bondar5471 <bondar54711745@gmail.com>
2018-06-07 13:42:17 +0300

    initial project version

---
+++ equation.rb
@@ -0,0 +1,28 @@
+
+def discriminant(a, b, c)
+  b ** 2 - 4 * a * c
+end
+
+puts 'Квадратное уравнение:'
+puts 'Введите коефициенты a, b и c'
+print 'a: '
+a = gets.chomp.to_i
+print 'b: '
+b = gets.chomp.to_i
+print 'c: '
+c = gets.chomp.to_i
+d = discriminant(a, b, c)
+if d < 0
+  puts "Дискриминант: #{d}"
+  puts 'Корней нет'
+elsif d == nil
+  x = b / (2 * a).to_f
+  puts "Дискриминант: #{d}"
+  puts "X = #{x}"
+else
+  x1 = (b + Math.sqrt(d)) / (2 * a).to_f
+  x2 = (b - Math.sqrt(d)) / (2 * a).to_f
+  puts "Дискриминант: #{d}"
+  puts "X1 = #{x1}"
+  puts "X2 = #{x2}"
+end
