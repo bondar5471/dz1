fbb22308994a792788aa517d02b32520f21b58fe
bondar5471 <bondar54711745@gmail.com>
2018-06-08 13:16:54 +0300

    mass style

--- area.rb
+++ area.rb
@@ -1,8 +1,7 @@
-puts "Введите основание трехугольника ?"
-a = gets.chomp
-  puts "Высота трехугольника?"
-h = gets.chomp
-  area=0.5*a.to_i*h.to_i
-
+puts "Введите основание треугольника ?"
+  a = gets.chomp
+puts "Высота треугольника?"
+  h = gets.chomp
+  area=0.5 * a.to_i*h.to_i
 puts "#{area} -площадь трехугольника "
 
--- mass.rb
+++ mass.rb
@@ -2,9 +2,9 @@ puts "Как вас зовут ?"
 name = gets.chomp
 puts "Ваш рост?"
 growth = gets.chomp
-cof=growth.to_i-110
+cof=growth.to_i - 110
 if cof<0
-  puts "#{name} Ваш вес уже оптимальный "
+  puts "#{name}, Ваш вес уже оптимальный "
 else
-  puts "#{name} Ваш вес не оптимальный "
-  end
+  puts "#{name},  Ваш вес не оптимальный "
+end
